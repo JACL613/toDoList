64e8629ce27d48566d2b61dfd9e5513b142fab75
JACL613 <74120137+JACL613@users.noreply.github.com>
2023-11-09 22:42:14 +0000

    estilos al formulario de registro

--- src/components/FormCreateUser.jsx
+++ src/components/FormCreateUser.jsx
@@ -3,6 +3,9 @@ import loginServices from '../services/loginServices'
 import { actionLogin } from '../actions/user.actions'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock, faSignature } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 export default function FormCreateUser () {
   const [firstName, setFirstName] = useState('')
@@ -22,17 +25,32 @@ export default function FormCreateUser () {
       })
   }
   return (
-    <form onSubmit={handleSubmitRegistration}>
-    <h2>Registrar</h2>
-        <label htmlFor='firstName'>First Name</label>
-        <input name='firstName' type="text" onChange={(e) => setFirstName(e.target.value)}/>
-        <label htmlFor='lastName'>Last Name</label>
-        <input name='lastName' type="text" onChange={(e) => setLastName(e.target.value)}/>
-        <label htmlFor='email'>Email</label>
-        <input name="email" type="email" onChange={(e) => setEmail(e.target.value)} autoComplete='username'/>
-        <label htmlFor="password">Password</label>
-        <input name='password' type="password" onChange={(e) => setPassword(e.target.value)} autoComplete='current-password'/>
-        <button type="submit">Registrar</button>
+    <form className='form' onSubmit={handleSubmitRegistration}>
+    <h2 id='heading'>Registrar</h2>
+        <label className='label-field' htmlFor='firstName'>First Name</label>
+        <div className='field'>
+        <FontAwesomeIcon className='input-icon' icon={faSignature}/>
+        <input className='input-field' name='firstName' type="text" onChange={(e) => setFirstName(e.target.value)}/>
+        </div>
+        <label className='label-field' htmlFor='lastName'>Last Name</label>
+        <div className='field'>
+        <FontAwesomeIcon className='input-icon' icon={faSignature}/>
+        <input className='input-field' name='lastName' type="text" onChange={(e) => setLastName(e.target.value)}/>
+        </div>
+        <label className='label-field' htmlFor='email'>Email</label>
+        <div className="field">
+        <FontAwesomeIcon className='input-icon' icon={faEnvelope}/>
+        <input className='input-field' name="email" type="email" onChange={(e) => setEmail(e.target.value)} autoComplete='username'/>
+        </div>
+        <label className='label-field' htmlFor="password">Password</label>
+        <div className="field">
+        <FontAwesomeIcon className='input-icon' icon={faLock}/>
+        <input className='input-field' name='password' type="password" onChange={(e) => setPassword(e.target.value)} autoComplete='current-password'/>
+        </div>
+        <div className='btn'>
+        <button className='button1' type="submit">Registrar</button>
+        <button className='button2' onClick={() => redirect('/login')}>Login</button>
+        </div>
     </form>
   )
 }
